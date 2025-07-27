1572f71bda5144337576f1b6949e67949cef9024
Devraj Prasad <119845770+Devraj-prasad07@users.noreply.github.com>
2025-07-27 23:04:43 +0530

    Time: 0 ms (100.00%) | Memory: 58.1 MB (40.18%) - LeetSync

--- 792-binary-search/binary-search.js
+++ 792-binary-search/binary-search.js
@@ -4,10 +4,17 @@
  * @return {number}
  */
 var search = function(nums, target) {
-    for(let i = 0; i < nums.length;i++){
-        if(nums[i] === target){
-            return i;
+    let right = nums.length-1;
+    let left = 0;
+    while (left <= right){
+        let middle = Math.floor((right + left)/2)
+        if(target === nums[middle]){
+            return middle;
+        } else if(target < nums[middle]){
+            right = middle - 1;
+        } else {
+            left = middle + 1;
         }
     }
-   return -1;
+    return -1;
 };
